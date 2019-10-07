04f8dec7f7c2bb412044bbd22c5b7a8d1333b029
caiorosseto <caiorosseto94@gmail.com>
2019-10-07 19:20:34 -0300

    inicial

---
+++ site/autores/caio/index.php
@@ -0,0 +1,20 @@
+<?php
+include('../../templates/header.php');
+?>
+    <main class="row m-1">
+        <div class="container">
+            
+            <div class="col-md-12 col-sm-12 m-1">
+                <img src="images/capa.jpeg" alt="capa divida entre três hqs de ficção cientifica" />
+                
+            </div>
+        
+        
+        </div>
+            
+        
+    
+    </main>
+<?php
+include('../../templates/footer.php');
+?>
