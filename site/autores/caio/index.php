54c7e9465275c64ecdf6f2df1fa2b869a52014fd
caiorosseto <caiorosseto94@gmail.com>
2019-10-07 20:20:42 -0300

    version_1

--- site/autores/caio/index.php
+++ site/autores/caio/index.php
@@ -1,20 +1,29 @@
 <?php
 include('../../templates/header.php');
 ?>
-    <main class="row m-1">
-        <div class="container">
-            
-            <div class="col-md-12 col-sm-12 m-1">
-                <img src="images/capa.jpeg" alt="capa divida entre três hqs de ficção cientifica" />
-                
+<main>
+    <div class="container">
+        <div class="row">
+            <div  class="col-md-3 col-sm-3 m-1">
+                <img src="images/capa_new.jpg" alt="capa divida entre três hqs de ficção cientifica" />
             </div>
-        
-        
+            <div class="col-md-3 col-sm-3 m-1">
+                <img src="images/matrix_300x150.jpg" alt="Capa da HQ Matix" />
+                <h2>Matrix</h2><br>
+                <p>Descrição do quadrinho Matrix</p>
+            </div>
+
+
+
+            <div class="col-md-3 col-sm-3 m-1">
+                <img src="images/Y_O_Ultimo_Homem_300x200.jpg" alt="capa do hq Y o último homem" />
+            </div>
         </div>
-            
-        
-    
-    </main>
+    </div>
+
+
+
+</main>
 <?php
 include('../../templates/footer.php');
 ?>
