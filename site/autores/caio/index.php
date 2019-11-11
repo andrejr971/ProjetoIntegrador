0248bf9c25d779f69d664dc0a88d9f1220259aff
caiorosseto <caiorosseto94@gmail.com>
2019-11-11 19:53:57 -0300

    Alteracoes Caio_v02

--- site/autores/caio/index.php
+++ site/autores/caio/index.php
@@ -1,25 +1,134 @@
 <?php
 include('../../templates/header.php');
+
 ?>
+
 <main>
+
     <div class="container">
         <div class="row">
-            <div  class="col-md-3 col-sm-3 m-1">
-                <img src="images/capa_new.jpg" alt="capa divida entre três hqs de ficção cientifica" />
+
+
+            <div class="col-sm">
+                <div class="zoom">
+                    <img class="img-responsive" src="images/capa_new.jpg" alt="capa divida entre três hqs de ficção cientifica" />
+                    <h2 class="text">CAPA</h2>
+
+                </div>
             </div>
-            <div class="col-md-3 col-sm-3 m-1">
-                <img src="images/matrix_300x150.jpg" alt="Capa da HQ Matix" />
+            <div class="col-sm">
+                <div class="zoom">
+                    <img class="img-responsive" src="images/matrix_300x150.jpg" alt="Capa da HQ Matix" />
+                </div>
                 <h2>Matrix</h2><br>
                 <p>Descrição do quadrinho Matrix</p>
             </div>
 
 
 
-            <div class="col-md-3 col-sm-3 m-1">
-                <img src="images/Y_O_Ultimo_Homem_300x200.jpg" alt="capa do hq Y o último homem" />
+            <div class="col-sm">
+                <div class="zoom">
+                    <img class="img-responsive" src="images/Y_O_Ultimo_Homem_300x200.jpg" alt="capa do hq Y o último homem" />
+                    <h2 class="text">CAPA</h2>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col">
+                <div class="btn-group">
+                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Alguma ação</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="btn-group">
+                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Alguma ação</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="btn-group">
+                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Alguma ação</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="btn-group">
+                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Alguma ação</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="btn-group">
+                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Alguma ação</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="btn-group">
+                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Alguma ação</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="btn-group">
+                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Alguma ação</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="btn-group">
+                    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="images/Paper_Girls80x80.jpg" />Botão grande
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Alguma ação</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    
+
 
 
 
