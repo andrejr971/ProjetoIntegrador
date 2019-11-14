da30813e3aa4e392321167cd544946438c84f1bb
andrejr971 <andre.souza64@fatec.sp.gov.br>
2019-11-13 21:27:14 -0300

    V4.7

---
+++ site/autores/wagner/index.php
@@ -0,0 +1,65 @@
+<?php
+include('../../templates/header.php');
+//Incluido o menu de navegação
+?>
+
+<main>
+    <div class="container">
+
+        <article>
+
+            <h1 class="text-center">Mangá</h1>
+
+            <div class="row text-center">
+
+                <div class="border rounded mb-3 col-12 col-md-6 col-lg-3">
+                    <a href="manga.php?manga=kimetsu">
+                        <img src="img/kimetsu0.jpg" class="mt-3 img-fluid" alt="Capa Kimetsu no Yaiaba">
+                        <h3>Kimetsu no Yaiba</h3>
+                        <p class="text-justify">Depois de ter sua família massacrada por um demônio,
+                            Tanjiro, o filho mais velho, sai em busca de uma cura para sua única irmã
+                            sobrevivente do massacre que acidentalmente foi transformada em um demônio.</p>
+                    </a>
+                </div>
+
+                <div class="border rounded mb-3 col-12 col-md-6 col-lg-3">
+                    <a href="manga.php?manga=nanatsu">
+                        <img src="img/nanatsu0.jpg" class="mt-3 img-fluid" alt="Capa Nanatsu no Taizai">
+                        <h3>Nanatsu no Taizai</h3>
+                        <p class="text-justify">Uma princesa decide ir atrás dos 7 pecados capitais,
+                            pois ela acredita que só eles possuem a força necessária para ajuda-lá a
+                            retomar o reino dos Cavaleiros Sagrados que encenaram um golpe de Estado
+                            e capturaram o Rei.</p>
+                    </a>
+                </div>
+
+                <div class="border rounded mb-3 col-12 col-md-6 col-lg-3">
+                    <a href="manga.php?manga=overlord">
+                        <img src="img/overlord0.jpg" class="mt-3 img-fluid" alt="Capa Overlord">
+                        <h3>Overlord</h3>
+                        <p class="text-justify">O protagonista é o último membro restante da Guilda de
+                            Ainz Ooal Gown. Inicialmente, costumava ser um salaryman, mas ao ser preso
+                            no jogo, decide se tornar a lenda do novo mundo.</p>
+                    </a>
+                </div>
+
+                <div class="border rounded mb-3 col-12 col-md-6 col-lg-3">
+                    <a href="manga.php?manga=boku">
+                        <img src="img/boku0.jpg" class="mt-3 img-fluid" alt="Capa Boku no Hero">
+                        <h3>Boku no Hero</h3>
+                        <p class="text-justify">All-Might, vendo que Izuku tem a atitude e o coração
+                            de um verdadeiro herói, mesmo sem possuir qualquer poder, resolve passá-lo
+                            seus poderes, a fim de torná-lo seu sucessor como o símbolo da paz.</p>
+                    </a>
+                </div>
+
+            </div>
+
+        </article>
+    </div>
+</main>
+
+<?php
+include('../../templates/footer.php');
+//Incluindo o rodapé
+?>
---
+++ site/autores/wagner/manga.php
@@ -0,0 +1,65 @@
+<?php
+if (!isset($_GET['manga'])) {
+    header('Location: index.php');
+} else {
+    $manga = $_GET['manga'];
+}
+
+include('../../templates/header.php');
+//Incluido o menu de navegação
+include('../../php/banco.php');
+
+?>
+
+<main>
+    <div class="container">
+        <div class="row">
+
+            <article class="col-12 col-md-9 col-lg-9">
+
+                <img class=" mt-3 mx-auto d-block img-fluid rounded" src="<?php echo $bancoManga[$manga]['img1'] ?>" alt="imagem de Izuku Midoriya com Mirio Togata">
+
+                <h1 class="text-center"><?php echo $bancoManga[$manga]['nome'] ?></h1>
+
+                <div class="text-justify">
+                    <p class="mt-2"><?php echo $bancoManga[$manga]['desc'] ?></div>
+
+                <div class="text-center">
+                    <img class="mb-3 img-fluid rounded" src="<?php echo $bancoManga[$manga]['img0'] ?>" alt="imagem de All Might">
+                    <img class="mb-3 img-fluid rounded" src="<?php echo $bancoManga[$manga]['img2'] ?>" alt="imagem de Izuku Midoriya">
+                </div>
+            </article>
+
+            <aside class="col-12 col-md-3 col-lg-3">
+                <h2 class="mt-1 text-center">Outros Links</h2>
+                <div class="p-3 mb-2 bg-light text-dark rounded text-center">
+                    <?php
+                    $nomePerson =  $bancoManga[$manga];
+                    shuffle($bancoManga);
+
+                    $arrayMais = new ArrayIterator($bancoManga);
+
+                    foreach (new LimitIterator($arrayMais, 0, 3) as $key => $value) {
+
+                        if ($nomePerson == $arrayMais[$key]) {
+                            unset($arrayMais[$key]);
+                        } else {
+                            ?>
+                            <a href="<?php $_SERVER['PHP_SELF']; ?>?manga=<?php echo $bancoManga[$key]['end']; ?>">
+                                <img class="rounded img-fluid mt-3" src="<?php echo $bancoManga[$key]['caminho']; ?>" alt="<?php echo $bancoManga[$key]['alt']; ?>">
+                            </a>
+                    <?php
+                        }
+                    }
+                    ?>
+                </div>
+            </aside>
+
+        </div>
+    </div>
+</main>
+
+<?php
+include('../../templates/footer.php');
+//Incluindo o rodapé
+?>
