0232ea5a8a455b7c0bc6d469381f5ebba1a20b74
andrejr971 <andre.souza64@fatec.sp.gov.br>
2019-12-02 13:08:05 -0300

    V 5.1

--- site/autores/andre/index.php
+++ site/autores/andre/index.php
@@ -16,7 +16,7 @@ include('../../php/banco.php');
                 </div>
                 <div class="col-12 col-md-4 col-lg-4 mb-2">
                     <div class="card">
-                        <a href="#" data-toggle="modal" data-target="#newDC0">
+                        <a href="#" data-toggle="modal" data-target="#new1">
                             <img src="img/new0.jpg" class="card-img-top w-100" alt="...">
                             <div class="card-body">
                                 <p class="card-text">Esquadrão Suicida | James Gunn revela foto do roteiro e indica trilha sonora</p>
@@ -26,7 +26,7 @@ include('../../php/banco.php');
                 </div>
                 <div class="col-12 col-md-4 col-lg-4 mb-2">
                     <div class="card">
-                        <a href="#" data-toggle="modal" data-target="#newDC1">
+                        <a href="#" data-toggle="modal" data-target="#new2">
                             <img src="img/new1.jpg" class="card-img-top w-100" alt="...">
                             <div class="card-body">
                                 <p class="card-text">DC divulga nova linha do tempo oficial</p>
@@ -36,7 +36,7 @@ include('../../php/banco.php');
                 </div>
                 <div class="col-12 col-md-4 col-lg-4 mb-2">
                     <div class="card">
-                        <a href="#" data-toggle="modal" data-target="#newDC2">
+                        <a href="#" data-toggle="modal" data-target="#new3">
                             <img src="img/new2.jpg" class="card-img-top w-100" alt="...">
                             <div class="card-body">
                                 <p class="card-text">Descoberto o possível novo Batman da DC Comics</p>
@@ -220,7 +220,82 @@ include('../../php/banco.php');
     </section>
 </div>
 
+<!--Modal !-->
+<div class="modal fade" id="new1" tabindex="-1" role="dialog" aria-labelledby="new1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Esquadrão Suicida | James Gunn revela foto do roteiro e indica trilha sonora</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src="img/new0.jpg" class="d-block w-50 offset-sm-3 mb-3 img-bordas" alt="">
+                <p class="text-justify">O diretor James Gunn compartilhou em seu Instagram uma foto do roteiro de Esquadrão Suicida, já criando expectativas para a trilha sonora do longa usando a frase da música "Blitzkrieg Bop", do Ramones.</p>
+                <p class="text-justify">Nas redes sociais, a atriz Mayling Ng compartilhou uma foto ao lado de Peter Capaldi, na qual os dois integrantes do elenco são vistos no que parece ser um estúdio, indicando a proximidade do início de filmagens.</p>
+                <p class="text-justify">O novo Esquadrão Suicida deve se inspirar nas HQs do grupo na década de 1980, escritas por Jon Ostrander e Kim Yale e deve contar com um novo elenco de vilões. O lançamento é previsto para em 6 de agosto de 2021.</p>
+                <p class="text-justify"><strong>Referência</strong></p>
+                <p class="text-justify"><a href="https://www.omelete.com.br/dc-comics/esquadrao-suicida-james-gunn-revela-foto-do-roteiro-e-indica-trilha-sonora" target="_blank">
+                        https://www.omelete.com.br/dc-comics/esquadrao-suicida-james-gunn-revela-foto-do-roteiro-e-indica-trilha-sonora</a>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="new2" tabindex="-1" role="dialog" aria-labelledby="new2" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">DC divulga nova linha do tempo oficial</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src="img/new1.jpg" class="d-block w-50 offset-sm-3 mb-3 img-bordas" alt="">
+                <p class="text-justify">Mudança estabelece Mulher-Maravilha como primeira heroína da editora.</p>
+                <p class="text-justify">Durante a New York Comic-Con 2019, a DC anunciou que lançará uma nova linha do tempo oficial que encaixe melhor as histórias da editora. Dan DiDio, editor-executivo da DC, explicou melhor as mudanças, que estabelecem a Mulher-Maravilha como ponto de partida para o universo heroico da editora.</p>
+                <p class="text-justify">De acordo com DiDio, a chegada de Diana aos Estados Unidos, antes da Segunda Guerra Mundial, estabelece a camada Primeira Geração de Super-Heróis. A segunda, chamada de Era Espacial, é marcada pela primeira aparição do Superman. A Terceira Geração é a Era das Crises e aborda as histórias publicadas entre Crise nas Infinitas Terras, publicada originalmente em 1985, e Ponto de Ignição, de 2011. Atualmente, a editora está na fase Flashpoint.</p>
+                <p class="text-justify">Vale lembrar que a minissérie Doomsday Clock, de Geoff Johns e Gary Frank, mostra a inclusão do universo Watchmen, criado por Alan Moore, ao cânone oficial da DC. Na história, o Batman investiga as constantes mudanças nos multiversos da editora, que podem ou não ter o envolvimento do Doutor Manhattan.Especula-se que, ao fim das doze edições, em dezembro, a nova timelineestará plenamente estabelecida.</p>
+                <p class="text-justify"><strong>Referência</strong></p>
+                <p class="text-justify"><a href="https://www.omelete.com.br/dc-comics/dc-divulga-nova-timeline-oficial" target="_blank">
+                        https://www.omelete.com.br/dc-comics/dc-divulga-nova-timeline-oficial</a>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="new3" tabindex="-1" role="dialog" aria-labelledby="new3" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Descoberto o possível novo Batman da DC Comics</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src="img/new2.jpg" class="d-block w-50 offset-sm-3 mb-3 img-bordas" alt="">
+                <p class="text-justify">A nova era do Batman na DC pode trazer mudanças significativas. Rumores recentes apontam para uma mudança na identidade do herói, que deixará de ser Bruce Wayne para ser Luke Fox, o atual Batwing.</p>
+                <p class="text-justify">Para quem não conhece, Luke Fox é filho de Lucius Fox, antigo colaborador das Empresas Wayne e grande aliado do Batman. Um gênio, o personagem acabou se tornando um membro importante da “família Batman” virando o herói Batwing. Porém, ao que tudo indica, seu papel na DC deve aumentar em breve.</p>
+                <p class="text-justify"><strong>Referência</strong></p>
+                <p class="text-justify"><a href="https://observatoriodocinema.bol.uol.com.br/artigos/2019/10/descoberto-o-possivel-novo-batman-da-dc-comics" target="_blank">
+                        https://observatoriodocinema.bol.uol.com.br/artigos/2019/10/descoberto-o-possivel-novo-batman-da-dc-comicsl</a>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
-include('../../templates/modalHome.php');
 include('../../templates/footer.php');
 ?>
