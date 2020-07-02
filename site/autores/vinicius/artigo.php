6ea80f63cb577019f681095d0cba4ff2d5c38b39
unknown <viniciusb90@gmail.com>
2020-07-02 10:10:50 -0300

    correção acessibilidade area vinicius

--- site/autores/vinicius/artigo.php
+++ site/autores/vinicius/artigo.php
@@ -1,45 +1,48 @@
 <?php
 include('../../templates/header.php');
 include('banco.php');
+
 ?>
 <main>
     <div class="container">
-        <article class="artigos--marvel">
+        <article class="artigos--marvel" aria-label="<?= $titulo; ?>">
             <div class="row m-3">
-                <div class="col-12 col-md-8 text-center">
+                <div class="col-12 text-center">
 
                     <div class="marvel-img">
-                        <img class="img-fluid rounded shadow-lg p-1 m-3 mx-auto d-block" src="<?php echo $imagem; ?>" alt="Capa <?php echo $value['titulo']; ?>">
-                        <h1 class="marvel-title"><?php echo $titulo; ?></h1>
+                        <img class="img-fluid rounded shadow-lg p-1 m-3 mx-auto d-block" src="<?= $imagem; ?>" alt="Capa <?= $value['titulo']; ?>">
+                        <h1 class="h2"><?= $titulo; ?></h1>
                     </div>
-                    <p class="text-justify"><?php echo $texto; ?></p>
-                </div>
-                <div class="col-12 col-md-4">
-                    <aside class="text-center">
-                        <div class="text-dark">
-                            <h2>Veja Também</h2>
-                        </div>
-
-                        <div class="row">
-                            <?php foreach (Posts('aleatorio') as $key => $value) { ?>
-                                <?php if ($value['id'] != $_GET['id']) { ?>
-                                    <div class="col-12 col-md-6">
-
-                                        <a href="artigo.php?id=<?php echo $value['id']; ?>">
-                                            <img class="img-fluid rounded" src="<?php echo $value['imagem']; ?>" alt="Capa <?php echo $value['titulo']; ?>">
-                                        </a>
-                                        <a href="artigo.php?id=<?php echo $value['id']; ?>">
-                                            <h2><?php echo $value['titulo']; ?></h2>
-                                        </a>
-                                    </div>
-                            <?php }
-                            } ?>
-                        </div>
-                    </aside>
+                    <p class="text-justify"><?= $texto; ?></p>
                 </div>
             </div>
-
         </article>
+
+        <section class="col-12">
+            <h2 class="h4 text-center">Veja Também</h2>
+            <div class="row">
+                <?php foreach (Posts() as $key => $value) { ?>
+                    <?php if ($value['id'] != $_GET['id']) { ?>
+
+                        <div class="col-md-4">
+                            <div class="row">
+                                <div class="col my-2">
+                                    <a href="artigo.php?id=<?= $value['id']; ?>">
+                                        <img class="img-fluid" src="<?= $value['imagem']; ?>" alt="Capa <?= $value['titulo']; ?>">
+                                    </a>
+                                </div>
+
+                                <div class="col my-2">
+                                    <a href="artigo.php?id=<?= $value['id']; ?>">
+                                        <h3 class="h5"><?= $value['titulo']; ?></h3>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                <?php }
+                } ?>
+            </div>
+        </section>
     </div>
 
 </main>
--- site/autores/vinicius/index.php
+++ site/autores/vinicius/index.php
@@ -1,53 +1,42 @@
 <?php
 include('../../templates/header.php');
+
 include('banco.php');
+
 //Incluido o menu de navegação
 ?>
 <main>
     <div class="container">
         <section>
-            <h2 class="text-center">Marvel</h2>
-            <div class="row">
-                <div class="col-12">
-                    <div class="card mt-2 mb-3">
-                        <div class="card-fundo-marvel"></div>
-                        <div class="avatar mx-auto">
-                            <img src="img/marvel.jpg" alt="Vinicius">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-12">
-                    <nav class="">
-                        <ul class="nav nav-pills flex-column flex-sm-row justify-content-center">
-
-                            <?php foreach (Posts('aleatorio') as $key => $value) { ?>
-                                <li class=""><a class="flex-sm-fill text-sm-center nav-link btn btn-outline-secondary mb-md-3" href="artigo.php?id=<?php echo $value['id']; ?>"><?php echo $value['titulo'] ?></a>
-                                </li>
-                            <?php } ?>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
+            <h1 class="text-center">Marvel</h1>
         </section>
 
-        <section class="row justify-content-center box-marvel text-center mt-3">
+        <section class="container">
+            <h2 class="h4 pl-3">Últimos Posts</h2>
 
+            <div class="container">
+                <?php foreach (Posts() as $key => $value) { ?>
 
-            <?php foreach (Posts() as $key => $value) { ?>
+                    <div class="row my-3">
 
-                <div class="col-12 col-md-4">
-                    <a href="artigo.php?id=<?php echo $value['id']; ?>">
-                        <div class="col-12">
-                            <img class="img-fluid rounded" src="<?php echo $value['imagem']; ?>" alt="Capa <?php echo $value['titulo']; ?>">
-                            <h4 class="m-1"><strong><?php echo $value['titulo']; ?></strong></h4>
-                            <p class="text-justify m-2"><?php echo substr($value['texto'], 0, 100) . '...'; ?></p>
-                        </div>
-                    </a>
-                </div>
-            <?php } ?>
+                        <article role="main">
+                            <div class="row">
+                                <div class="col-md-4 d-flex justify-content-center">
+                                    <img class="img-fluid rounded" src="<?= $value['imagem']; ?>" alt="Capa <?= $value['titulo']; ?>">
+                                </div>
+
+                                <div class="col-md-5">
+                                    <h4 class="m-1"><strong><?= $value['titulo']; ?></strong></h4>
+                                    <p class="text-justify m-2"><?= substr($value['texto'], 0, 100) . '...'; ?></p>
+                                    <a href="artigo.php?id=<?= $value['id']; ?>"><button type="button" class="btn btn-dark btn-block">Ler Mais</button></a>
+                                </div>
+                            </div>
+                        </article>
+
+                    </div>
+
+                <?php } ?>
+            </div>
         </section>
     </div>
 
